5822e2905202ac19476591d4ab162428b71636f3
xkrumeng <guoliehua@gmail.com>
2019-05-20 18:36:50 +0800

    'commot'

--- src/components/Previewer/index.js
+++ src/components/Previewer/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
-
+import hljs from 'highlight.js'
+import javascript from 'highlight.js/lib/languages/javascript';
+import './preview.css'
 
 class Previewer extends React.Component {
   constructor(props) {
     super(props)
   }
 
+  componentDidMount() {
+    hljs.registerLanguage('javascript', javascript)
+  }
 
   render() {
     return (
-      <div className="panel" dangerouslySetInnerHTML={{__html: this.props.previewText}}></div>
+      <div className="panel">
+        <div className="preview" dangerouslySetInnerHTML={{__html: this.props.previewText}}></div>
+      </div>
     )
   }
 }
